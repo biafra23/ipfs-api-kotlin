37f7dff508b751b652d1d7e581e027b0327b81a8
Dirk Jäckel <github@dirk.jaeckel.name>
2025-04-25 01:05:41 +0200

    upgrading to moshi 1.9.1

--- lib/src/main/kotlin/io/ipfs/kotlin/defaults/Defaults.kt
+++ lib/src/main/kotlin/io/ipfs/kotlin/defaults/Defaults.kt
@@ -1,10 +1,11 @@
 package io.ipfs.kotlin.defaults
 
 import com.squareup.moshi.Moshi
-import io.ipfs.kotlin.commands.*
-import io.ipfs.kotlin.model.MessageWithCode
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 
-internal  fun createOKHTTP() = OkHttpClient.Builder().build()
+internal fun createOKHTTP() = OkHttpClient.Builder().build()
 
-internal fun createMoshi() = Moshi.Builder().build()
+internal fun createMoshi() = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory()) // Add support for Kotlin data classes
+    .build()
