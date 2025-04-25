0fc132dc28ecadc41ab4126053dfe64371c04609
Dirk Jäckel <github@dirk.jaeckel.name>
2025-04-25 17:41:20 +0200

    WIP: pin/ls https://docs.ipfs.tech/reference/kubo/rpc/#api-v0-pin-ls

--- lib/src/main/kotlin/io/ipfs/kotlin/commands/Pins.kt
+++ lib/src/main/kotlin/io/ipfs/kotlin/commands/Pins.kt
@@ -1,8 +1,15 @@
 package io.ipfs.kotlin.commands
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonClass
 import io.ipfs.kotlin.IPFSConnection
 
 class Pins(val ipfs: IPFSConnection) {
+
+    private val pinsAdapter: JsonAdapter<PinLsResult> by lazy {
+        ipfs.config.moshi.adapter(PinLsResult::class.java)
+    }
 
     fun add(hash: String): Boolean {
         val resultString = ipfs.callCmd("pin/add/$hash").use { it.string() }
@@ -13,12 +20,54 @@ class Pins(val ipfs: IPFSConnection) {
         return resultBoolean
     }
 
-    fun ls(): Boolean {
-        val resultString = ipfs.callCmd("pin/ls").use { it.string() }
-        val resultBoolean = resultString.isNotEmpty()
-        if (!resultBoolean) {
-            ipfs.setErrorByJSON(resultString)
+    fun ls(): PinLsResultResult {
+        val httpResponse = ipfs.executeCmd("pin/ls")
+
+        if (httpResponse.isSuccessful) {
+            val result = pinsAdapter.fromJson(httpResponse.body()!!.use {
+                it.string()
+            })
+            return PinLsResultResult.Success(result!!)
+        } else {
+            ipfs.setErrorByJSON(httpResponse.body()!!.use { it.string() })
+            return PinLsResultResult.Failure(ipfs.lastError?.Message ?: "Unknown error")
         }
-        return resultBoolean
     }
 }
+
+sealed class PinLsResultResult {
+    data class Success(val pinLsResult: PinLsResult) : PinLsResultResult()
+    data class Failure(val errorMessage: String) : PinLsResultResult()
+}
+
+@JsonClass(generateAdapter = true)
+data class PinLsResult(
+    @Json(name = "PinLsList")
+    val pinLsList: PinLsList? = null,
+    @Json(name = "PinLsObject")
+    val pinLsObject: PinLsObject? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class PinLsList(
+    @Json(name = "Keys")
+    val keys: Map<String, PinDetails>? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class PinDetails(
+    @Json(name = "Name")
+    val name: String? = null,
+    @Json(name = "Type")
+    val type: String? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class PinLsObject(
+    @Json(name = "Cid")
+    val cid: String? = null,
+    @Json(name = "Name")
+    val name: String? = null,
+    @Json(name = "Type")
+    val type: String? = null
+)
--- lib/src/test/kotlin/io/ipfs/kotlin/TestPins.kt
+++ lib/src/test/kotlin/io/ipfs/kotlin/TestPins.kt
@@ -1,5 +1,6 @@
 package io.ipfs.kotlin
 
+import io.ipfs.kotlin.commands.PinLsResultResult
 import okhttp3.mockwebserver.MockResponse
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -40,4 +41,41 @@ class TestPins : BaseIPFSWebserverTest() {
 
     }
 
+    @Test
+    fun testLsPin() {
+        // setup
+        server.enqueue(MockResponse().setBody("""{
+  "PinLsList": {
+    "Keys": {
+      "<string>": {
+        "Name": "<string>",
+        "Type": "<string>"
+      }
+    }
+  },
+  "PinLsObject": {
+    "Cid": "<string>",
+    "Name": "<string>",
+    "Type": "<string>"
+  }
+}
+
+"""))
+
+        // invoke
+        val result = ipfs.pins.ls()
+
+        // assert
+        assertThat(result).isNotNull()
+        assertThat(result).isInstanceOf(PinLsResultResult.Success::class.java)
+        assertThat((result as PinLsResultResult.Success).pinLsResult).isNotNull()
+        assertThat((result).pinLsResult.pinLsList?.keys?.keys?.size).isEqualTo(1)
+//        assertThat((result).pinLsResult.pinLsList?.keys?.keys?.[0].name).isEqualTo("<string>")
+ //       assertThat((result).pinLsResult.peers!![0].addr).isEqualTo("/ip4/104.156.227.102/tcp/4001")
+
+        assertThat(ipfs.lastError).isNull()
+
+        val executedRequest = server.takeRequest()
+        assertThat(executedRequest.path).isEqualTo("/pin/ls")
+    }
 }
